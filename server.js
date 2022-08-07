cdf9e5ee433c84a003de40961879eb4a1ae7426b
Fabrizio-Turdo <fabrizioturdo1999@gmail.com>
2022-08-07 15:41:48 -0300

    prueba2

--- server.js
+++ server.js
@@ -1,11 +1,16 @@
 const express = require('express')
 
 const app = express()
+console.log(__dirname)
 
 app.use(express.static(__dirname+'/public'))
 
 app.get('./',(req,res)=>{
     res.render('index.html')
+})
+
+app.get('/prueba',(req,res)=>{
+    console.log('Prueba heroku')
 })
 
 const PORT = process.env.PORT || 8081
